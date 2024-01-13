ade2de60597a3d305e6ebfd04d570ad29e169e5c
mzffrtp <mzffr.tpgl@gmail.com>
2024-01-13 21:16:55 +0100

    data ingestion

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const client = require("./elasticsearch/client");
+const data = require("./data_managenent/retrieve_and_ingest_data")
 
 const app = express();
 dotenv.config({
     path: process.cwd() + "/.env"
 })
+
+app.use("/ingest_data", data)
 
 const port = process.env.HTTP_SERVER_PORT || 7003;
 const host = process.env.HTTP_SERVER_HOST || "localhost";
